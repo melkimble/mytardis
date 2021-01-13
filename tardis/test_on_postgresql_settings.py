c642c296b3f408888d603f6ae59bdee8c44225da
Dmitry Yakhnov <dmitry@yakhnov.info>
2021-01-13 16:33:40 +1100

    Revert back last change

--- tardis/test_on_postgresql_settings.py
+++ tardis/test_on_postgresql_settings.py
@@ -1,6 +1,5 @@
 # pylint: disable=wildcard-import,unused-wildcard-import
 from tardis.test_settings import *  # noqa # pylint: disable=W0401,W0614
-from tardis.default_settings.apps_default_settings import *  # pylint: disable=C0411,C0413
 
 DATABASES = {
     'default': {
